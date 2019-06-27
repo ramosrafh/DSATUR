eb9c87afe15f3c912a37f57eb5a471247ed4eb69
ramosrafh <ramosrafh@gmail.com>
2019-06-27 17:34:38 -0300

    Update DSATUR.py

--- DSATUR.py
+++ DSATUR.py
@@ -1,3 +1,5 @@
+import csv
+
 class Vertice:
 	def __init__(self, n):
 		self.nome = n
@@ -88,32 +90,44 @@ class Grafo:
 		for key in sorted(list(self.vertices.keys())):
 			print(key + ' ' + str(self.vertices[key].cor) + ' ' + str(self.vertices[key].adjacentes))
 
+# MAIN
+
 
 
 g = Grafo()
-for i in range(ord('A'), ord('K')):
-	g.add_vertice(Vertice(chr(i)))
+#for i in range(ord('A'), ord('K')):
+#	g.add_vertice(Vertice(chr(i)))
 
-arestas = ['AB', 'AC', 'AJ', 'BC', 'BG', 'BD', 'BJ', 'JD', 'JG', 'GH', 'IG', 'IH', 'ID', 'IC', 'FC', 'FE', 'FG', 'GE', 'EC', 'HJ', 'HI', 'DC']
+arestas = []
+
+
+with open('grafo.csv', newline='') as csvfile:
+	csv_reader = csv.reader(csvfile, delimiter=',')
+	csv_reader = list(csv_reader)
+
+	#print(csv_reader[0][3])
+	
+	for coluna in csv_reader:
+		g.add_vertice(Vertice(coluna[0]))
+	
+	loop = int(len(g.vertices))
+
+	for i in range(loop):
+		for col in csv_reader[i][1:]:
+			arestas.append(col)
+			i += 1
+
+
+#arestas = ['AB', 'AC', 'AJ', 'BC', 'BG', 'BD', 'BJ', 'JD', 'JG', 'GH', 'IG', 'IH', 'ID', 'IC', 'FC', 'FE', 'FG', 'GE', 'EC', 'HJ', 'HI', 'DC']
+
 for aresta in arestas:
 	g.add_aresta(aresta[:1], aresta[1:])
 
 g.mostrar_grafo()
-
 print('')
-
-#g.pintar(g.max_len())
-g.pintar('C')
-
+g.pintar(g.max_len())
 print('')
-
 g.mostrar_grafo()
-
-#print(g.max_adj('H'))
-
-#print (str(g.vertices[F].adjacentes))
-
-
 
 
 
